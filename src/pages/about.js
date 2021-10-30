63119f4e7fae3e8a19c604b2336386a35e139613
anirudhpanda <anirudhpandaofficial@gmail.com>
2021-10-30 11:58:24 +0530

    layout component and about page tuts

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>I am Anirudh and I ama fuckerrrr</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
